86e9a9df4313eb4110fb07ce313799ddb22722e8
Erick Martins <ericknmp@gmail.com>
2018-09-12 11:27:46 -0300

    Created project on studio

---
+++ loyaldesignsystem/src/main/java/br/com/grupoltm/loyaldesignsystem/atoms/Icon.java
@@ -0,0 +1,16 @@
+package br.com.grupoltm.loyaldesignsystem.atoms;
+
+import android.annotation.SuppressLint;
+import android.util.Log;
+
+public class Icon {
+
+    private static final String TAG = "LOYAL_DESIGN_SYSTEM_ATOM_ICON";
+
+
+    @SuppressLint("LongLogTag")
+    public static void d(String message){
+        Log.d(TAG, message);
+    }
+
+}
